a8bda09a12b7c925d5c4846eae7292cbd3a29020
Faisel Khan <125124484+faizkhan1318@users.noreply.github.com>
2024-01-31 14:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function(){
-	const browserName = getName();
-	const browserVersion = getVersion();
+let user = navigator.userAgent;
+let user2 =navigator.appName;
 
-	const browserData = document.getElementById("browser-info");
-	browserData.innerText = "You are using " + browserName + " version " + browserVersion;
-	
-})
-function getName() {
-    const userAgent = navigator.userAgent;
+let user3 = navigator.appVersion;
 
-    if (userAgent.includes("Chrome")) {
-        return "Google Chrome";
-    } else if (userAgent.includes("Firefox")) {
-        return "Mozilla Firefox";
-    } else if (userAgent.includes("Safari")) {
-        return "Apple Safari";
-    } else if (userAgent.includes("Edge")) {
-        return "Microsoft Edge";
-    } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
-        return "Opera";
-    } else if (userAgent.includes("Trident") || userAgent.includes("MSIE")) {
-        return "Internet Explorer";
-    } else {
-        return "Unknown Browser";
-    }
-}
 
-function getBrowserVersion() {
-    const userAgent = navigator.userAgent;
-    const versionIndex = userAgent.indexOf('Version/');
 
-    if (versionIndex !== -1) {
-        return userAgent.substring(versionIndex + 8, userAgent.indexOf(' ', versionIndex));
-    } else {
-        return "Unknown Version";
-    }
-}
+const display = document.getElementById("browser-info");
+display.innerText = "You are using " +user2 +" version "+user3;
